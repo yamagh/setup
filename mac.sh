ed86ebacd59bff098cf203b82b2466b0f134e9ac
yamagh <hiroyuki.yamagami@gmail.com>
2017-04-27 20:24:44 +0900

    Initial commit

---
+++ mac.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+brew install ghq
+brew install hub
+brew install peco
+brew install tmux
+
+brew cask install appcleaner
+brew cask install bitbar
+brew cask install michaelvillar-timer
+
