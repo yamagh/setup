7d6b1f9b7d2c7b4ea3968e7b8bddc983b766bb3b
yamagh <hiroyuki.yamagami@gmail.com>
2017-05-01 23:46:12 +0900

    Add jdownloader

--- mac.sh
+++ mac.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+brew update
+brew upgrade
+
 brew install ghq
 brew install hub
 brew install peco
+brew install terminal-notifier
 brew install tmux
 
 brew cask install appcleaner
 brew cask install bitbar
 brew cask install michaelvillar-timer
+brew cask install jdownloader
 
+brew cask cleanup
