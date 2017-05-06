3a11982c1e67ac1460754818047c85b1e1d5cee4
yamagh <hiroyuki.yamagami@gmail.com>
2017-05-07 00:40:38 +0900

    Add ImageMagick

--- mac.sh
+++ mac.sh
@@ -8,6 +8,10 @@ brew install hub
 brew install peco
 brew install terminal-notifier
 brew install tmux
+brew install lua
+brew install vim --with-lua
+brew install macvim --with-lua
+brew install imagemagick
 
 brew cask install appcleaner
 brew cask install bitbar
