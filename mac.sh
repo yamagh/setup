153e97c465a7056e7c3c7dbaaf30b33bdd5fe412
yamagh <hiroyuki.yamagami@gmail.com>
2018-11-08 23:13:33 +0900

    Change something

--- mac.sh
+++ mac.sh
@@ -12,6 +12,8 @@ brew install lua
 #brew install vim --with-lua
 brew install macvim --with-lua --with-python3 --with-override-system-vim
 brew install imagemagick
+brew install rbenv
+brew install pyenv
 
 brew cask install appcleaner
 brew cask install bitbar
@@ -19,9 +21,13 @@ brew cask install michaelvillar-timer
 brew cask install jdownloader
 
 brew tap dteoh/sqa
+brew cask install slate
 brew cask install slowquitapps
+brew cask install clipy
 
 brew cask cleanup
 
 pip install todotxt-machine
+
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
